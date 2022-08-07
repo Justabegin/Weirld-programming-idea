8d7bb8d71e0e808d0275065923d9a84870a8c055
Justabegin <108687789+Justabegin@users.noreply.github.com>
2022-08-07 15:48:43 +0800

    Add files via upload
    Algorithm to reverse a linklist

---
+++ reverse-linklist.py
@@ -0,0 +1,29 @@
+linklist = ['b','a','e','c','d']
+startpointer = 2
+linklistpointer = [3,0,5,4,2]
+# Find the position of last element in the linklist
+def findfinal(arraypointer,start):
+    length = len(arraypointer)
+    for i in range(length):
+        if arraypointer[start] !=5 :
+            start = arraypointer[start]
+    return start
+endpointer = findfinal(linklistpointer,startpointer)
+startpointer = endpointer
+def changecontentoflinklistpointer(arraypointer,endpointer):
+    length = len(arraypointer)
+    linklistpointer1 = [length,length,length,length,length]
+    for i in range(length-1) :
+        for i in range(length):
+            if arraypointer[i] == endpointer :
+                linklistpointer1[endpointer] = i
+                endpointer =  i
+    return linklistpointer1        
+newlinklistpointer = changecontentoflinklistpointer(linklistpointer,endpointer)
+print(endpointer)
+print(newlinklistpointer)
+newlinklistpointerfinal = findfinal(newlinklistpointer,endpointer)
+print(newlinklistpointerfinal)
+
+
+
